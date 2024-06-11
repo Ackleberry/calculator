d104b5bb20a2560ed196f3dafea97df3dc1002de
Brooks Anderson <bilbrobaggins@gmail.com>
2024-06-11 15:47:42 -0500

    Add operate function

--- calculator.js
+++ calculator.js
@@ -14,3 +14,27 @@ function div(a, b) {
     return a / b;
 }
 
+function operate(operator, a, b) {
+
+    let res;
+
+    switch(operator) {
+        case '+': 
+            res = add(a, b);
+        break;
+        case '-':
+            res = sub(a, b);
+        break;
+        case '*':
+            res = mult(a, b);
+        break;
+        case '/':
+            res = div(a, b);
+        break;
+        default:
+            console.log("ERROR: Unknown operator.");
+        break;
+    }
+
+    return res;
+}
