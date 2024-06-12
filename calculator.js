2315d04e2fe9790f971fbc6b19a31caba4d15dc1
Brooks Anderson <bilbrobaggins@gmail.com>
2024-06-12 10:43:30 -0500

    Update logic

--- calculator.js
+++ calculator.js
@@ -30,10 +30,14 @@ const calc = {
     div: function(a, b) {
         return a / b;
     },
+    mod: function(a, b) {
+        return a % b;
+    },
     isOperator: function(op) {
         return (op === '+' || op === '-' || op === '*' || op === '/' || op === '%')
     },
     operate: function(op, a, b) {
+        console.log(`OP: ${op}, A: ${a}, B: ${b}`);
         let res;
         switch(op) {
             case '+': 
@@ -48,6 +52,9 @@ const calc = {
             case '/':
                 res = this.div(a, b);
             break;
+            case '%':
+                res = this.mod(a,b);
+            break;
             default:
                 console.log("ERROR: Unknown operator.");
             break;
@@ -60,37 +67,52 @@ const calc = {
 function btnHandler(event) {
     let btnValue = event.target.innerText;
 
-    switch(calc.state) {
-        case States.STATE_IDLE:
-            if (!calc.isOperator(btnValue)) {
-                display.textContent = btnValue;
-                calc.state = States.STATE_FIRST_NUM;
-            }
-        break;
-        case States.STATE_FIRST_NUM:
-            if (calc.isOperator(btnValue)) {
-                calc.state = States.STATE_SECOND_NUM;
-                calc.operator = btnValue;
-                calc.num1 = Number(display.textContent);
-                display.textContent = '';
-            } else {
-                display.textContent += btnValue;
-            }
-        break;
-        case States.STATE_SECOND_NUM:
-            if (btnValue === '=') {
-                calc.state = States.STATE_EVAL;
-                calc.num2 = Number(display.textContent);
-                calc.result = calc.operate(calc.operator, calc.num1, calc.num2);
-                display.textContent = calc.result;
-                calc.state = States.STATE_IDLE;
-            } else {
-                display.textContent += btnValue;
-            }
-        break;
-        default:
-            console.log("ERROR: Unknown calculator state!");
-        break;
+    if (btnValue === 'CLR') {
+        calc.reset();
+        display.textContent = '';
+    } else if (btnValue === 'DEL') {
+        display.textContent = display.textContent.slice(0, -1);
+    } else {
+        switch(calc.state) {
+            case States.STATE_IDLE:
+                if (!calc.isOperator(btnValue)) {
+                    display.textContent = btnValue;
+                    calc.state = States.STATE_FIRST_NUM;
+                }
+            break;
+            case States.STATE_FIRST_NUM:
+                if (calc.isOperator(btnValue)) {
+                    calc.state = States.STATE_SECOND_NUM;
+                    calc.operator = btnValue;
+                    calc.num1 = Number(display.textContent);
+                } else {
+                    display.textContent += btnValue;
+                }
+            break;
+            case States.STATE_SECOND_NUM:
+                if (calc.isOperator(btnValue)) {
+                    calc.num2 = Number(display.textContent);
+                    calc.result = calc.operate(calc.operator, calc.num1, calc.num2);
+                    calc.operator = btnValue;
+                    calc.num1 = calc.result;
+                    display.textContent = calc.result;
+                } else if (btnValue === '=') {
+                    calc.num2 = Number(display.textContent);
+                    calc.result = calc.operate(calc.operator, calc.num1, calc.num2);
+                    display.textContent = calc.result;
+                    calc.state = States.STATE_IDLE;
+                } else {
+                    if (display.textContent == calc.num1) {
+                        display.textContent = btnValue;
+                    } else {
+                        display.textContent += btnValue;
+                    }
+                }
+            break;
+            default:
+                console.log("ERROR: Unknown calculator state!");
+            break;
+        }
     }
 }
 
